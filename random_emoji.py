fb7f54bf30f5431fe64fd9e4643d89a9a4f4fd07
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2015-03-10 16:30:17 -0700

    Copy chosen emoji to the clipboard.

--- random_emoji.py
+++ random_emoji.py
@@ -1,9 +1,29 @@
 #!/usr/bin/env python3
 
+import platform
 import random
+import subprocess
 
 import requests
 
+
+def copy_to_clipboard(text):
+    if platform.system() == 'Linux':
+        cmd = ['xsel', '-b']
+    elif platform.system() == 'Darwin':
+        cmd = ['pbcopy']
+    else:
+        return  # Don't know about this OS.
+    proc = subprocess.Popen(cmd, stdin=subprocess.PIPE)
+    proc.communicate(text.encode('utf-8'))
+    if proc.returncode != 0:
+        raise RuntimeError('Failed to copy to clipboard.')
+
+
 result = requests.get('https://api.github.com/emojis').json()
 
-print(':{}:'.format(random.choice(list(result.keys()))))
+choice = random.choice(list(result.keys()))
+
+print(':{}:'.format(choice))
+
+copy_to_clipboard(':{}:'.format(choice))
